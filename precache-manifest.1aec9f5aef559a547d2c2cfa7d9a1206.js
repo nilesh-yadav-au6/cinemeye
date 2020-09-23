cbab39c0ffc15c8654930d0e30810c1b9f6791e1
Nilesh Yadav <nilesh221990@gmail.com>
2020-09-23 18:51:14 +0530

    Updates

---
+++ precache-manifest.1aec9f5aef559a547d2c2cfa7d9a1206.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038d40be715966e029f87e8f940fb5a1",
+    "url": "/nilesh-yadav-au6/cinemeye.git/index.html"
+  },
+  {
+    "revision": "2622c0e01944d14919d8",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cb2f8b3ec0354d8fa513",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/css/main.8fe3811b.chunk.css"
+  },
+  {
+    "revision": "2622c0e01944d14919d8",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/js/2.e132e9ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/js/2.e132e9ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2f8b3ec0354d8fa513",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/js/main.e83f36aa.chunk.js"
+  },
+  {
+    "revision": "666b31fa138981829d53",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/js/runtime-main.e2582eca.js"
+  },
+  {
+    "revision": "67cfeb3b4b367b54a4260af276c8caa2",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/adventure.67cfeb3b.jpg"
+  },
+  {
+    "revision": "2fae7ec0e45f54ae14d07142435e9cc1",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/comedy.2fae7ec0.jpg"
+  },
+  {
+    "revision": "c04d2269214966645d7e2458dfa3302d",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/drama.c04d2269.jpg"
+  },
+  {
+    "revision": "d53a1275d30a7ebe5aebdbf070864dd7",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/episode4.d53a1275.jpg"
+  },
+  {
+    "revision": "7065f19f26f15177ab96a14519159bc4",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/gentlemen.7065f19f.jpg"
+  },
+  {
+    "revision": "e02508fefeff8365cb2bea06107f5017",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/image1.e02508fe.jpg"
+  },
+  {
+    "revision": "9382fad4b3fa5a711b1c7ac953e0a34d",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/jumanji.9382fad4.jpg"
+  },
+  {
+    "revision": "cfe89f12612312d86c66ff4e32258de4",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/logo.cfe89f12.png"
+  },
+  {
+    "revision": "515385dbea9a742db6e6d3c109bb052c",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/midway.515385db.jpg"
+  },
+  {
+    "revision": "0c5594185249a95e7f920336a09bb845",
+    "url": "/nilesh-yadav-au6/cinemeye.git/static/media/westworld.0c559418.jpg"
+  }
+]);
