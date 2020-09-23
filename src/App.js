8bdfd1b608676a0e576cd6bed80da2336aea11ec
Nilesh Yadav <nilesh221990@gmail.com>
2020-09-23 19:02:42 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Switch ,Route } from "react-router-dom"
+import { Switch ,Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./components/HomePage"
 import TvseriesPage from "./components/TvseriesPage"
@@ -10,12 +10,10 @@ function App() {
   return (
     <div className="App">
         <Navbar />
-        <HashRouter>
         <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/tvseries" component={TvseriesPage} />
         </Switch>
-        </HashRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
     
   </React.StrictMode>,
   document.getElementById('root')
